8c831fe2244cb08bfc1d5b4b9dd7d35946308c38
IlyaMBondarev <60775995+IlyaMBondarev@users.noreply.github.com>
2022-02-05 19:53:11 +0300

    Add files via upload

---
+++ public/scripts/search-form.js
@@ -0,0 +1,42 @@
+import { renderBlock } from './lib.js';
+import * as dayjs from './dayjs';
+const oneDayValue = 1000 * 60 * 60 * 24;
+export function renderSearchFormBlock(enterDate = dayjs(Date.now() + oneDayValue).format('YYYY-MM-DD'), exitDate = dayjs(Date.now() + 3 * oneDayValue).format('YYYY-MM-DD')) {
+    const lastDateOfTheMonth = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate();
+    const minDate = dayjs().format('YYYY-MM-DD');
+    const maxDate = dayjs(new Date(new Date().getFullYear(), new Date().getMonth(), lastDateOfTheMonth)).format('YYYY-MM-DD');
+    renderBlock('search-form-block', `
+    <form>
+      <fieldset class="search-filedset">
+        <div class="row">
+          <div>
+            <label for="city">Город</label>
+            <input id="city" type="text" disabled value="Санкт-Петербург" />
+            <input type="hidden" disabled value="59.9386,30.3141" />
+          </div>
+          <!--<div class="providers">
+            <label><input type="checkbox" name="provider" value="homy" checked /> Homy</label>
+            <label><input type="checkbox" name="provider" value="flat-rent" checked /> FlatRent</label>
+          </div>--!>
+        </div>
+        <div class="row">
+          <div>
+            <label for="check-in-date">Дата заезда</label>
+            <input id="check-in-date" type="date" value="${enterDate}" min="${minDate}" max="${maxDate}" name="checkin" />
+          </div>
+          <div>
+            <label for="check-out-date">Дата выезда</label>
+            <input id="check-out-date" type="date" value="${exitDate}" min="${minDate}" max="${maxDate}" name="checkout" />
+          </div>
+          <div>
+            <label for="max-price">Макс. цена суток</label>
+            <input id="max-price" type="text" value="" name="price" class="max-price" />
+          </div>
+          <div>
+            <div><button>Найти</button></div>
+          </div>
+        </div>
+      </fieldset>
+    </form>
+    `);
+}
---
+++ public/scripts/user.js
@@ -0,0 +1,15 @@
+import { renderBlock } from './lib.js';
+export function renderUserBlock(userName, avatarURL, favoriteItemsAmount) {
+    const hasFavoriteItems = favoriteItemsAmount > 0;
+    renderBlock('user-block', `
+    <div class="header-container">
+      <img class="avatar" src="${avatarURL}" alt="${userName}" />
+      <div class="info">
+          <p class="name">${userName}</p>
+          <p class="fav">
+            <i class="heart-icon${hasFavoriteItems ? ' active' : ''}"></i>${favoriteItemsAmount || 'ничего нет'}
+          </p>
+      </div>
+    </div>
+    `);
+}
